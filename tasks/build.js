71abd6f36859a0433d9d57b31aa9b93368607480
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-10-25 12:17:39 -0700

    Clean up assets before build

--- tasks/build.js
+++ tasks/build.js
@@ -1,5 +1,15 @@
+const rimraf = require('rimraf');
 const compile = require('./compile');
 
-(async () => {
-  await compile();
-})();
+console.log("Clearing old assets...")
+rimraf('./site/assets', {}, e => {
+  if (e) {
+    console.error(e)
+  } else {
+    console.log("All clear!");
+    (async () => {
+
+      await compile();
+    })();
+  }
+});
