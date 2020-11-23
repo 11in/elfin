f22fc131716a31c684135901d8d08a1ea5ef0708
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-11-22 16:21:15 -0800

    Minify css on prod builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
-module.exports = {
-    plugins: [
+module.exports = () => {
+    const isProd = process.env.NODE_ENV === 'production';
+
+    let plugins = [
         require('postcss-import'),
         require('postcss-preset-env'),
         require('tailwindcss'),
-    ]
+    ];
+
+    if (isProd) {
+        plugins.push(require('cssnano'));
+    }
+
+    return {
+        plugins: plugins,
+    };
 }
