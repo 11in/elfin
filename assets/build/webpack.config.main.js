bdae392e01c2ae2e7f8d6d21ab126f8687e65d9f
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-12-18 22:51:05 -0800

    Serve webpack dev server content from right loc

--- assets/build/webpack.config.main.js
+++ assets/build/webpack.config.main.js
@@ -13,7 +13,7 @@ module.exports = {
     },
 
     devServer: {
-        contentBase: path.join(__dirname, '..', 'dist'),
+        contentBase: path.join(__dirname, '..', '..', 'dist'),
         compress: true,
         port: 9000,
         watchContentBase: true,
